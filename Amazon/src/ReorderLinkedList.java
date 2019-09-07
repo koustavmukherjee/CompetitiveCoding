ff387521e5ac220c3c759a7eb06ea56e18a4b7f9
koustavmukherjee <mukherjee.koustav@gmail.com>
2019-09-07 19:09:25 -0400

    Added question and solution

--- Amazon/src/ReorderLinkedList.java
+++ Amazon/src/ReorderLinkedList.java
@@ -1,5 +1,16 @@
 import java.util.Stack;
 
+/**
+ * 
+ * @author mukhe
+Given x1 -> x2 ->x3…-> y3 -> y2 -> y1
+Reorder it to x1 -> y1 -> x2 -> y2 -> x3 -> y3 …..
+
+e.g.
+Given 1 -> 2 -> 3 -> 4 -> 5
+Return 1 -> 5 -> 2 -> 4 -> 3
+
+ */
 public class ReorderLinkedList {
 	public LinkedNode reorder(LinkedNode head) {
 		if(head == null)
